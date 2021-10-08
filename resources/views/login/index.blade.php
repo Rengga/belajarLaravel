ad4b1f1ff7d05ad6073315a4710f6753fa17d74c
ngidolMaki <renggaarnanta@gmail.com>
2021-10-08 17:03:46 +0700

    menambakan user login dan middleware

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,19 +1,43 @@
 @extends('layout.main')
 @section('container')
-<main class="form-signin">
-    <form>
-  
-      <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Email address</label>
-      </div>
-      <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingPassword">Password</label>
-      </div>
-  
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    </form>
-    <p class="d-block text-center mt-3"><a href="/register">Register</a></p>
-  </main>
+<div class="row justify-content-center">
+  <div class="col-md-4">
+    @if (session()->has('sukses'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      {{ session('sukses') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
+    @if (session()->has('loginError'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      {{ session('loginError') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+    
+
+    <main class="form-signin">
+      <form action="/login" method="post">
+        <div class="form-floating">
+          @csrf
+          <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+          <label for="email">Email address</label>
+          @error('email')
+        <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+        </div>
+        
+        <div class="form-floating">
+          <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+          <label for="password" required="">Password</label>
+        </div>
+    
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      </form>
+      <p class="d-block text-center mt-3"><a href="/register">Register</a></p>
+    </main>
+  </div>
+</div>
+
 @endsection
